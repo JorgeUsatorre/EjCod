91435a261039b343cb8574f8ae48fd86e3eb0c6b
JorgeUsatorre <usatorre1210@gmail.com>
2022-10-26 09:23:56 +0200

    Ejercicio COD

---
+++ boletin2_2_EjCod/src/Main.java
@@ -0,0 +1,13 @@
+
+package boletin2_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Proyecto que calcula el area de un cuadrado de lado=3
+        float lado = 3, area;
+        area = lado*lado;
+        System.out.println("area ="+area);
+    }
+
+}
