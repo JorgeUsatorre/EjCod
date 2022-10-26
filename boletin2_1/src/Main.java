f06d3c195a2e74631d9e2e5c145643470f453d38
JorgeUsatorre <usatorre1210@gmail.com>
2022-10-26 09:07:28 +0200

    Ejercicio COD

---
+++ boletin2_1/src/Main.java
@@ -0,0 +1,22 @@
+
+package boletin2_1;
+
+import java.util.Scanner;
+
+
+public class Main{
+
+
+    public static void main(String[] args) {
+        float base , altura; //Variables locales;
+        Scanner obxScaner = new Scanner(System.in);
+        System.out.println("teclea la base");
+        base=obxScaner.nextFloat();
+        System.out.println("teclea la altura");
+        altura = obxScaner.nextFloat();
+        float area = base*altura;
+        System.out.println("el area ="+area);
+
+    }
+
+}
